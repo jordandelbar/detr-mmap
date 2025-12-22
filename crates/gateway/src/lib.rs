f8a1918158fc8800adc7172497349f674ea73d39
Jordan Delbar <jordandelbar@gmail.com>
2025-12-22 23:09:40 +0100

    refactor: Logging apart

--- crates/gateway/src/lib.rs
+++ crates/gateway/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod camera;
 pub mod config;
+pub mod logging;
 
 pub use camera::*;
--- crates/gateway/src/main.rs
+++ crates/gateway/src/main.rs
@@ -1,28 +1,7 @@
-use gateway::{
-    camera::run_camera,
-    config::{Environment, get_configuration},
-};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use gateway::{camera::run_camera, config::get_configuration, logging::setup_logging};
 
 fn main() {
     let config = get_configuration().expect("failed to load configuration");
-    let log_level = config.log_level.as_str();
-    let env_filter =
-        tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
-    let registry = tracing_subscriber::registry().with(env_filter);
-
-    match config.environment {
-        Environment::Production => {
-            registry
-                .with(tracing_subscriber::fmt::layer().json().with_level(true))
-                .init();
-        }
-        Environment::Development => {
-            registry
-                .with(tracing_subscriber::fmt::layer().pretty().with_ansi(true))
-                .init();
-        }
-    }
-
+    setup_logging(config);
     let _ = run_camera();
 }
