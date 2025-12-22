5d6765fd4a7714ef1223730e34ae7012aa06f7a3
Jordan Delbar <jordandelbar@gmail.com>
2025-12-22 20:09:43 +0100

    refactor: Remove semaphore code

--- crates/bridge/src/lib.rs
+++ crates/bridge/src/lib.rs
@@ -1,9 +1,7 @@
 pub mod errors;
 pub mod mmap_reader;
 pub mod mmap_writer;
-pub mod semaphore;
 
 pub use errors::BridgeError;
 pub use mmap_reader::MmapReader;
 pub use mmap_writer::FrameWriter;
-pub use semaphore::{Semaphore, SemaphoreError};
